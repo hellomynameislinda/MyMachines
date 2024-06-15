0eba9d871dcc8eaf3c3733953cb80f1043787d33
Linda <linda@hellomynameislinda.com>
2024-06-15 21:16:20 +0200

    Fixed required fields, add-button etc.

--- MyMachines/Entities/Machine.cs
+++ MyMachines/Entities/Machine.cs
@@ -1,10 +1,16 @@
-﻿namespace MyMachines.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyMachines.Entities
 {
     public class Machine
     {
         public Guid Id { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "The name must exceed three characters")]
         public string Name { get; set; }
+        [Required]
         public bool Status { get; set; }
+        [Required]
         public DateTime DateAdded { get; set; } = DateTime.Now;
     }
 }
